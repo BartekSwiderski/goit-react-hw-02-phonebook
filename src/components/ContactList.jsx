32c547f12d6004fd6dcca1d76f24c149ad8df6b1
Bartek Świderski <bartekzg@gmail.com>
2021-12-08 15:15:57 +0100

    added render contact list

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,22 +1,24 @@
 import styles from "./Phonebook.module.css";
 
 const ContactList = ({ filter, del, data }) => {
-  return (
-    <ul className={styles.contacts} style={{ padding: 0 }}>
-      {data.map(({ id, name, number }) => {
-        return name.toLowerCase().includes(filter.toLowerCase()) ? (
-          <li key={id} className={styles.contact}>
-            {name} {number}
-            <button className={styles.btn__delete} onClick={() => del(id)}>
-              Delete
-            </button>
-          </li>
-        ) : (
-          <></>
-        );
-      })}
-    </ul>
-  );
+  if (data.length !== 0) {
+    return (
+      <ul className={styles.contacts} style={{ padding: 0 }}>
+        {data.map(({ id, name, number }) => {
+          return name.toLowerCase().includes(filter.toLowerCase()) ? (
+            <li key={id} className={styles.contact}>
+              {name} {number}
+              <button className={styles.btn__delete} onClick={() => del(id)}>
+                Delete
+              </button>
+            </li>
+          ) : (
+            <></>
+          );
+        })}
+      </ul>
+    );
+  } else return <p>You have no saved contacts</p>;
 };
 
 export default ContactList;
